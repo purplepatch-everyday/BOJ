99d9c8ba7d11b61ecebac5b668fd92956218afa0
purplepatch-everyday <80674430+purplepatch-everyday@users.noreply.github.com>
2022-11-29 20:27:52 +0900

    A + B - 2

---
+++ 2558.py
@@ -0,0 +1,20 @@
+"""
+2558번: A + B - 2
+
+문제: 2 정수를 받아 더한 값을 출력한다
+
+input: 
+정수 
+정수
+
+output: 두개의 정수를 더한 값 출력하기 
+
+"""
+
+# input 받기 
+input_num1 = input()
+input_num2 = input()
+
+result = int(input_num1) + int(input_num2)
+
+print(result)
