6a0b64636f388070ad94dbd185955d2ff287c809
purplepatch-everyday <80674430+purplepatch-everyday@users.noreply.github.com>
2022-11-30 16:27:20 +0900

    큰 수 A+B

---
+++ 10757.py
@@ -0,0 +1,17 @@
+"""
+10757번: 큰 수 A+B  
+
+문제: 두 정수 A 와 B를 입력받은 다음, A + B를 출력하기
+
+input: 첫번째 줄에 두 정수 A 와 B 가 주어진다
+
+
+output: 두정수를 더한 값 출력 
+"""
+
+# 입력 받기 
+input_num1,input_num2 = map(int,input().split())
+
+result = input_num1 + input_num2
+
+print(result)
